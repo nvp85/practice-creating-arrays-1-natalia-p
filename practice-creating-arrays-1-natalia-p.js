e85633d1db856a63516771002af7e945247c29d3
nvp85 <nvp@zhukov.al>
2025-03-11 23:19:01 -0500

    complete the tasks

---
+++ practice-creating-arrays-1-natalia-p.js
@@ -0,0 +1,21 @@
+// Task 1: Create Arrays
+// An array of 5 grocery store items.
+const groceryItems = ["Bell Pepper", "Salsa", "Bread", "Eggs", "Cheese"];
+// An array of 3 numbers representing prices.
+const prices = [2.50, 1.49, 5.89];
+
+// Task 2: Access and Log the Values
+// 1. Log the entire array of grocery items.
+console.log("Grocery Items:", groceryItems);
+
+// 2. Log the entire array of prices.
+console.log("Prices:", prices);
+
+// 3. Log three individual items of your choice.
+console.log("First item:", groceryItems[0]); 
+console.log("Third item:", groceryItems[2]); 
+console.log("Fifth item:", groceryItems[4]);
+
+// 4. Log two prices of your choice
+console.log("First price:", prices[0]);
+console.log("Second price:", prices[1]);
